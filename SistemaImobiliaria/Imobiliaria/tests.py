82165099b1d828594993cf0874a41e71d6856d74
adriano <adriano@debian>
2017-06-05 09:56:18 -0300

    Update the view novo_empreendimento and update the template novo_empreendimento.html. In addition, also insert firsts two tests

--- SistemaImobiliaria/Imobiliaria/tests.py
+++ SistemaImobiliaria/Imobiliaria/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+# test novo_empreendimento 
+class NovoEmpreendimentoGet(TestCase):
+	def setUp(self):
+		self.resp = self.client.get('/imobiliaria/novo_empreendimento')
+
+	def test_get(self):
+		self.assertEqual(200, self.resp.status_code)
+
+	def test_template(self):
+		self.assertTemplateUsed(self.resp, 'imobiliaria/novo_empreendimento.html')
